f3afdd37ade92a1a84ec42da80b41976866ddd20
harlemtwist <walter_smith@yahoo.com>
2013-11-07 22:15:35 +0000

    next

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Worldxxxx omg!');
+
+  var idxbuf = fs.readFileSync("index.html");
+  response.send(idxbuf.toString());
 });
 
 var port = process.env.PORT || 5000;
